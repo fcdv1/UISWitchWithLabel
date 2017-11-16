b9e52b5059b7b20a03d5716a9ade3938d53065f6
zfh <245185601@qq.com>
2017-11-16 14:36:37 +0800

    Initial Commit

---
+++ SwitchWithLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwitchWithLabel
+//
+//  Created by CIA on 2017/11/16.
+//  Copyright © 2017年 CIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
